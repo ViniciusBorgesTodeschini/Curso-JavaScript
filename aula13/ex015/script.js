0655eae1548fc992b48fa53a610dec18c2255b47
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2020-12-13 20:32:58 -0300

    Update script.js

--- aula13/ex015/script.js
+++ aula13/ex015/script.js
@@ -3,12 +3,39 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length ==0 || Number(fano.value > ano)) {
+    if (fano.value.length ==0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tento novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade>=0 && idade < 12){
+                //criança
+            } else if (idade < 21){
+                //jovem
+            } else if (idade < 60){
+                //adulto
+            } else {
+                //idoso
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade>=0 && idade < 12){
+                //criança
+            } else if (idade < 21){
+                //jovem
+            } else if (idade < 60){
+                //adulto
+            } else {
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 
 }
