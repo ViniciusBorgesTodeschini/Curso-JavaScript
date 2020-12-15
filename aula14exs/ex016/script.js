fc7dd17c71b170f143284435f433aa6530106363
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2020-12-15 11:07:01 -0300

    complementando

--- aula14exs/ex016/script.js
+++ aula14exs/ex016/script.js
@@ -1,12 +1,27 @@
-function contar(){
-    var ini = window.document.getElementById('txtini')
-    var fim = window.document.getElementById('txtfim')
-    var passo = window.document.getElementById('txtpas')
-    var res = window.document.getElementById('res')
+function contar() {
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (ini.value.lenght ==0 || fim.value.lenght == 0 || passo.value.lenght == 0){
-        window.alert ('[ERRO] Faltam dados! Preencha todos os campos corretamente.')
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        window.alert('[ERRO] Faltam dados! Preencha todos os campos corretamente.')
     } else {
-        alert('Tudo ok!')
+       res.innerHTML = 'Contando: '
+       let i = Number(ini.value) 
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+       if (i < f) {
+           // Contagem crescente
+            for (let c = i; c <= f; c +=p ){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
