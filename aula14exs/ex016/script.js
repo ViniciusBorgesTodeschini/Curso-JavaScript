a2256b3f065ab676bb6bdf3a34594a91582697a8
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2020-12-15 12:26:38 -0300

    criando

--- aula14exs/ex016/script.js
+++ aula14exs/ex016/script.js
@@ -5,13 +5,18 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        // Não está fucionando! O JS está considerando campo vazio com o valor O.
         window.alert('[ERRO] Faltam dados! Preencha todos os campos corretamente.')
         res.innerHTML = 'Impossível contar!'
     } else {
-       res.innerHTML = 'Contando: '
+       res.innerHTML = 'Contando: <br>'
        let i = Number(ini.value) 
        let f = Number(fim.value)
        let p = Number(passo.value)
+       if (p <= 0){
+            window.alert ('Passo inválido! Considerando PASSO 1')
+            p = 1
+       }
        if (i < f) {
            // Contagem crescente
             for (let c = i; c <= f; c +=p ){
---
+++ aula14exs/ex017/script.js
@@ -0,0 +1,10 @@
+function tabuad(){
+    let num = window.document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.lenght == 0){
+        window.alert ('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+    }
+}
